db09ff26b771b5a6d36e47781be400dbdeaa68a9
tanya <tanya@TATYANANEDO11BD>
2024-08-05 03:12:17 +0300

    Repeats fixed

--- Arrays/Arrays2/main.cpp
+++ Arrays/Arrays2/main.cpp
@@ -3,28 +3,30 @@ using namespace std;
 
 char tab = '\t';
 
+#define REPEATS
 #define SORT
 
 void main()
 {
 	setlocale(LC_ALL, "");
 
-	const int N = 5;	//Количество строк/столбцов
-	int Arr[N][N] = {};
+	const int ROWS = 3;	//Количество строк/столбцов
+	const int COLS = 8;	//Количество строк/столбцов
+	int Arr[ROWS][COLS] = {};
 
 	//Заполнение случайными числами
-	for (int i = 0; i < N; i++)
+	for (int i = 0; i < ROWS; i++)
 	{
-		for (int j = 0; j < N; j++)
+		for (int j = 0; j < COLS; j++)
 		{
-			Arr[i][j] = rand() % 100;
+			Arr[i][j] = rand() % 10;
 		}
 	}
 
 	//Вывод исходного массива:
-	for (int i = 0; i < N; i++)
+	for (int i = 0; i < ROWS; i++)
 	{
-		for (int j = 0; j < N; j++)
+		for (int j = 0; j < COLS; j++)
 		{
 			cout << Arr[i][j] << tab;
 		}
@@ -32,18 +34,60 @@ void main()
 	}
 	cout << "\n=============================================\n";
 
+	
+#ifdef REPEATS
+	//Поиск повторяющихся значений
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			bool metBefore = false;
+			int enter = 1;
+			for (int k = 0; k <= i; k++)
+			{
+				for (int l = 0; l < (k == i ? j : COLS); l++)
+				{
+					if (Arr[i][j] == Arr[k][l])
+					{
+						metBefore = true;
+						break;
+					}
+				}
+			}
+			if (metBefore)continue;
+
+			for (int k = i; k < ROWS; k++)
+			{
+				int l;
+				for ((k == i) ? l = j + 1 : l = 0; l < COLS; l++)
+				{
+					if (Arr[i][j] == Arr[k][l])
+					{
+						enter++;
+					}
+				}
+			}
+
+			if (enter > 1)cout << Arr[i][j] << " встречается " << enter << " раз." << endl;
+		}
+	}
+	cout << "\n=============================================\n";
+#endif // REPEATS
+
+
+
 #ifdef SORT
 
 	//Сортировка:
-	for (int i = 0; i < N; i++)
+	for (int i = 0; i < ROWS; i++)
 	{
-		for (int j = 0; j < N; j++)
+		for (int j = 0; j < COLS; j++)
 		{
-			for (int k = i; k < N; k++)
+			for (int k = i; k < ROWS; k++)
 			{
 				//int l;
 				//for ((k == i) ? l = j + 1 : l = 0; l < N; l++)
-				for (int l = k == i ? j + 1 : 0; l < N; l++)
+				for (int l = k == i ? j + 1 : 0; l < COLS; l++)
 				{
 					int temp;
 					if (Arr[k][l] < Arr[i][j])
@@ -58,52 +102,14 @@ void main()
 	}
 
 	//Вывод:
-	for (int i = 0; i < N; i++)
+	for (int i = 0; i < ROWS; i++)
 	{
-		for (int j = 0; j < N; j++)
+		for (int j = 0; j < COLS; j++)
 		{
 			cout << Arr[i][j] << tab;
 		}
 		cout << endl;
 	}
 #endif // SORT
-
-	cout << "\n=============================================\n";
-
-	//Поиск повторяющихся значений
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < N; j++)
-		{
-			bool metBefore = false;
-			int enter = 1;
-			for (int k = 0; k <= i; k++)
-			{
-				for (int l = 0; l < (k == i ? j : N); l++)
-				{
-					if (Arr[i][j] == Arr[k][l])
-					{
-						metBefore = true;
-						break;
-					}
-				}
-			}
-			if (metBefore)continue;
-
-			for (int k = 0; k <= i; k++)
-			{
-				int l;
-				for ((k == i) ? l = j + 1 : l = 0; l < N; l++)
-				{
-					if (Arr[i][j] == Arr[k][l])
-					{
-						enter++;
-					}
-				}
-			}
-
-			if (enter > 1)cout << Arr[i][j] << " встречается " << enter << " раз." << endl;
-		}
-	}
 
 }
