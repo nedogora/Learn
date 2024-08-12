f5fd0ae1ad18998ed23af0fa2d561f9dc69b4124
tanya <tanya@TATYANANEDO11BD>
2024-08-13 01:42:21 +0300

    Power in one line

--- Functions/Recursion/main.cpp
+++ Functions/Recursion/main.cpp
@@ -1,23 +1,16 @@
-﻿/*
-1. Factorial(); // возвращает факториал числа
-2. Power(); // возводит число в степень
-3. Fibonacci(); //выводит на экран ряд Фибоначчи до указанного предела
-4. Fibonacci(); // выводит на экран заданное количество членов из ряда Фибоначчи
-
-Все функции должны быть реализованы рекурcивно
-*/
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
 #define tab '\t'
 
+int Factorial(int n);	// Факториал числа
+double Power(double n, double d);	// Возвденеие числа в степень
+void FibonacciTo(int n, int a = 0, int b = 1);	// Выводи на экран ряда Фибоначчи до указанного предела
+void Fibonacci(int n, int a = 0, int b = 1);	// Вывод на экран заданного количества членов из ряда Фибоначчи
+
 //#define FACTORIAL
 //#define POWER
-
-int Factorial(int n);
-double Power(double n, double d);
-void Fibonacci(int n, int a = 0, int b = 1);
-int Fibonacci(int n, int a = 0, int b = 1);
+#define FIBONACCI
 
 void main()
 {
@@ -38,41 +31,44 @@ void main()
 	cout << n << " ^ " << d << " = " << Power(n, d) << endl;
 #endif // POWER
 
+#ifdef FIBONACCI
 	int n;
-	cout << "Введите число: "; cin >> n;
-	Fibonacci(n);
-	cout << endl;
+	cout << "Введите максимальное значение: "; cin >> n;
+	FibonacciTo(n);
+#endif // FIBONACCI
 }
 
 int Factorial(int n)
 {
-	if (n == 0) return 0;
-	if (n == 1) return n;
-	return n *= Factorial(n - 1);
+	/*if (n == 0) return 1;
+	//if (n == 1) return n;
+	return n *= Factorial(n - 1);*/
+	return n == 0 ? 1 : n * Factorial(n - 1);
 }
 
 double Power(double n, double d)
 {
-	if (d == 0) return 1;
+	/*if (d == 0) return 1;
 	if (d < 0)
 	{
 		n = 1 / n;
 		n *= (-1);
 	}
-	
-	return n * Power(n, d - 1);
+
+	return n * Power(n, d - 1);*/
+	return (d == 0 ? 1 : (d < 0 ? -(1 / n) : n) * Power(n, d - 1));
+}
+
+void FibonacciTo(int n, int a, int b)
+{
+	if (a > n) return;
+	cout << a << tab;
+	FibonacciTo(n, b, a + b);
 }
 
 void Fibonacci(int n, int a, int b)
 {
-	if (a > n) return;
-	cout << a << tab;
-	Fibonacci(n, b, a + b);
-}
-
-int Fibonacci(int n, int a, int b)
-{
-	if (n == 0) return 0;
+	if (n == 0) return;
 	cout << a << tab;
 	Fibonacci(n - 1, b, a + b);
 }
