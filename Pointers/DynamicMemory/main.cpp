36d7e70a94110264fcb7a5ba7794f01dc5c03dc8
tanya <tanya@TATYANANEDO11BD>
2024-08-20 02:15:53 +0300

    TODO: split

---
+++ Pointers/DynamicMemory/main.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+void FillRand(int Arr[], const int n);
+void Print(int Arr[], const int n);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int n;
+	cout << "¬ведите количество элементов массива: "; cin >> n;
+	int* Arr = new int[n];
+
+	FillRand(Arr, n);
+	Print(Arr, n);
+
+	delete[] Arr;
+}
+
+void FillRand(int Arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		Arr[i] = rand() % 100;
+	}
+}
+
+void Print(int Arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << Arr[i] << '\t';
+	}
+	cout << endl;
+}
